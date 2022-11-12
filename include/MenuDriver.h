54bcb09056140a38dcba8a2369fcea9abba7db5c
Davit Khudaverdyan <davkhuda@gmail.com>
2022-11-12 16:11:44 -0500

    Implement printing rotation logic for paginated printing.

--- include/MenuDriver.h
+++ include/MenuDriver.h
@@ -26,19 +26,29 @@ private:
 	std::map<int, Position> team_style;
 	State cur_state {State::Intro};
 	
+	size_t pagination_size {3};
+	size_t start_rotation_display {0};
+	
 	std::vector<Team> get_slice(size_t from, size_t to);
 		
 	// Print functions
 	void print_menu();
 	void print_intro();
 	void print_overview();
-	void print_rotations();
-	void print_rotations(bool paginated, unsigned int page_size); //TODO
+	void print_rotations_intro();
+	void print_all_rotations();
+	void print_rotations(bool direction); // true -> next, false -> previous
 	
 	// Input functions
 	bool is_valid_input(char input);
 	void handle_input();
+	void handle_input_Intro(char input);
+	void handle_input_Overview(char input);
+	void handle_input_ShowRotations(char input);
+	void handle_input_Exit(char input);
 	void perform_input_action(char input);
+	
+	void change_pagination_size();
 	
 public:
 	void run();
--- src/MenuDriver.cpp
+++ src/MenuDriver.cpp
@@ -13,6 +13,12 @@ MenuDriver::MenuDriver(Team team, std::vector<Team> rotations, std::map<int, Pos
 	
 }
 
+void MenuDriver::run() {
+	cur_state = State::Intro;
+	print_intro();
+	print_menu();
+	handle_input();
+}
 
 void MenuDriver::print_intro() {
 	std::cout << "=-=-=-=-=-=-=- Welcome to the Volleyball Rotation Viewer! =-=-=-=-=-=-=- " << std::endl;
@@ -33,25 +39,14 @@ void MenuDriver::print_overview() {
 	}
 }
 
-void MenuDriver::print_rotations() {
-	std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=- Team Rotations =-=-=-=-=-=-=-=-=-=-=-=-=- " << std::endl;
+void MenuDriver::print_rotations_intro(){
 	
-	for(size_t i = 0; i < rotations.size(); i++) {
-		std::cout << "----- Rotation # " << i+1 << "-----" << std::endl;
-		rotations.at(i).show_team();
-		std::cout << "-----------------------------------" << std::endl;
+	std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=- Team Rotations =-=-=-=-=-=-=-=-=-=-=-=-=- " << std::endl;
+	std::cout << "|\tThe team provided is capable of running " << rotations.size() << " rotations" << std::endl;
+	std::cout << "|\twith the team composition of:" << std::endl;
+	for (auto comp  : team_style) {
+		std::cout << "Position: " << comp.first << " " << position_as_str(comp.second) << std::endl;
 	}
-}
-
-void print_rotations(bool paginated, unsigned int page_size) {
-		std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=- Team Rotations =-=-=-=-=-=-=-=-=-=-=-=-=- " << std::endl;
-}
-
-void MenuDriver::run() {
-	cur_state = State::Intro;
-	print_intro();
-	print_menu();
-	handle_input();
 }
 
 bool MenuDriver::is_valid_input(char input){
@@ -60,21 +55,36 @@ bool MenuDriver::is_valid_input(char input){
 }
 
 void MenuDriver::perform_input_action(char input) {
+	switch (cur_state) {
+		case State::Intro:
+			handle_input_Intro(input);
+			break;
+		case State::Overview:
+			handle_input_Overview(input);
+			break;
+		case State::ShowRotations:
+			handle_input_ShowRotations(input);
+			break;
+		case State::Exit:
+			handle_input_Exit(input);
+			break;
+		default:
+			break;
+	}
+}
+
+void MenuDriver::handle_input_Intro(char input) {
 	switch (input) {
 		case 'i':
-			cur_state = State::Intro;
 			print_intro();
 			break;
 		case 'o':
 			cur_state = State::Overview;
 			print_overview();
 			break;
-		case 'a':
-			cur_state = State::ShowRotations;
-			print_rotations();
-			break;
 		case 's':
 			cur_state = State::ShowRotations;
+			print_rotations_intro();
 			break;
 		case 'q':
 			cur_state = State::Exit;
@@ -85,6 +95,54 @@ void MenuDriver::perform_input_action(char input) {
 	handle_input();
 }
 
+void MenuDriver::handle_input_ShowRotations(char input) {
+	switch (input) {
+		case 'i':
+			cur_state = State::Intro;
+			print_intro();
+			break;
+		case 'o':
+			cur_state = State::Overview;
+			print_overview();
+			break;
+		case 'a':
+			print_all_rotations();
+			break;
+		case 's':
+			change_pagination_size();
+			break;
+		case 'n':
+			print_rotations(true);
+			break;
+		case 'N':
+			print_rotations(false);
+			break;
+		case 'q':
+			cur_state = State::Exit;
+			return;
+	}
+
+	print_menu();
+	handle_input();
+}
+
+void MenuDriver::change_pagination_size() {
+	std::cout << "Input new pagination size: ";
+	std::cin >> pagination_size;
+	if (pagination_size > rotations.size()) {
+		pagination_size = rotations.size();
+	} else if (pagination_size <= 0 ) {
+		pagination_size = 1; 
+	}
+}
+
+void MenuDriver::handle_input_Overview(char input) {
+	handle_input_Intro(input);
+}
+
+void MenuDriver::handle_input_Exit(char input) {
+	return;
+}
 
 void MenuDriver::handle_input() {
 	char input {};
@@ -98,7 +156,53 @@ void MenuDriver::handle_input() {
 			std::cout << "Invalid input! \n" << std::endl;
 		}
 	}
+}
+
+
+void MenuDriver::print_rotations(bool direction) {
+	// true -> next, false -> prev
+	static bool first_call {true};
+	size_t to {};
+	if(!first_call){
+		if (direction) { // next rotations
+			if(pagination_size >= rotations.size()){
+				// Since the pagination size is greater than the entire vector
+				// set the starting point to zero, showing the entire vector
+				start_rotation_display = 0;
+			} else if(!(start_rotation_display + pagination_size >= rotations.size())) {
+				start_rotation_display += pagination_size;
+			}
+		} else { // previous rotations
+			// Clamp the start to the zero index if going backwords would be negative
+			// Even though size_t is unsigned, good to check in case of underflow
+			if(start_rotation_display < pagination_size) {
+				start_rotation_display = 0;
+			} else {
+				start_rotation_display -= pagination_size;
+			}
+		}
+	}
+	first_call = false;
 	
+	to = start_rotation_display + pagination_size;
+	if (to >= rotations.size()){
+		to = rotations.size();
+	}
+	for(size_t i = start_rotation_display; i < to; i++){
+		std::cout << "--------- Rotation # " << i + 1 << " of " << rotations.size() << std::endl;
+		rotations.at(i).show_team();
+		std::cout << "-----------------------------------" << std::endl;
+	}
+}
+
+void MenuDriver::print_all_rotations() {
+	std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=- Team Rotations =-=-=-=-=-=-=-=-=-=-=-=-=- " << std::endl;
+	
+	for(size_t i = 0; i < rotations.size(); i++) {
+		std::cout << "--------- Rotation # " << i + 1 << " of " << rotations.size() << std::endl;
+		rotations.at(i).show_team();
+		std::cout << "-----------------------------------" << std::endl;
+	}
 }
 
 void MenuDriver::print_menu() {
@@ -109,11 +213,9 @@ void MenuDriver::print_menu() {
 			std:: cout << "\n\n>>| i: intro, o: overview, s: showteams, q: exit" << std::endl;
 			break;
 		case State::ShowRotations:
-			std:: cout << "\n\n>>| i: intro, o: overview, a: print all rotations, s: set number of rotations to show, n: next rotations, N: previous rotations q: exit" << std::endl;
+			std:: cout << "\n\n>>| i: intro, o: overview, a: print all rotations, s: set number of rotations to show\nn: next rotations, N: previous rotations q: exit" << std::endl;
 			break;
 		default:
 			break;
 	}
 }
-
-
